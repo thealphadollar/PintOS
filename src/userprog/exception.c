f56c14ae9ba2cc4d50636987b11768412a26b950
thealphadollar <thealphadollar>
2020-03-31 15:16:56 +0530

    add memory usage

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -1,9 +1,11 @@
 #include "userprog/exception.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <debug.h>
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/syscall.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -151,11 +153,22 @@ page_fault (struct intr_frame *f)
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
-  kill (f);
+//   printf ("Page fault at %p: %s error %s page in %s context.\n",
+//           fault_addr,
+//           not_present ? "not present" : "rights violation",
+//           write ? "writing" : "reading",
+//           user ? "user" : "kernel");
+//   kill (f);
+   if (user) {
+      // process exiting
+      exit(-1);
+      NOT_REACHED();
+   }
+   else {
+      f->eip = (void (*) (void)) f->eax;
+      f->eax = (uint32_t) 0xffffffff;
+
+      return;
+   }
 }
 
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,8 +4,14 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/synch.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+
+// helper functions
+static int get_byte(const uint8_t *unsigned_addr);
+static bool put_byte (uint8_t *unsigned_dst, uint8_t byte);
+static uint32_t get_word (const uint32_t *unsigned_addr);
 
 void
 syscall_init (void) 
@@ -19,47 +25,49 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  uintptr_t *esp = f->esp;
+  uint32_t *esp = f->esp;
   // use switch case to invoke corresponding system call
-  switch(*esp)
+  switch(get_word(esp))
   {
     case SYS_EXIT:
-      exit((int) *((esp)+1));
+      exit((int) get_word((esp)+1));
       break;
     case SYS_CREATE:
-      f->eax = create((const char *) *((esp)+1), 
-                            (unsigned) *((esp) + 2));
+      f->eax = create((const char *) get_word((esp)+1), 
+                            (unsigned) get_word((esp) + 2));
       break;
     case SYS_REMOVE:
-      f->eax = remove((const char *) *((esp)+1));
+      f->eax = remove((const char *) get_word((esp)+1));
       break;
     case SYS_OPEN:
-      f->eax = open((const char *) *((esp)+1));
+      f->eax = open((const char *) get_word((esp)+1));
       break;
     case SYS_FILESIZE:
-      f->eax = filesize((int) *((esp)+1));
+      f->eax = filesize((int) get_word((esp)+1));
       break;
     case SYS_READ:
-      f->eax = read((int) *((esp)+1), 
-                      (void *) *((esp)+2),
-                      (unsigned) *((esp) + 3));
+      f->eax = read((int) get_word((esp)+1), 
+                      (void *) get_word((esp)+2),
+                      (unsigned) get_word((esp) + 3));
       break;
     case SYS_WRITE:
-      f->eax = write((int) *((esp)+1), 
-                      (void *) *((esp)+2),
-                      (unsigned) *((esp) + 3));
+      f->eax = write((int) get_word((esp)+1), 
+                      (void *) get_word((esp)+2),
+                      (unsigned) get_word((esp) + 3));
       break;
     case SYS_CLOSE:
-      close((int) *((esp)+1));
+      close((int) get_word((esp)+1));
       break;
   }
   thread_exit ();
+  // make sure this part is never reached
+  NOT_REACHED();
 }
 
 // exit system call
 static void
 exit (int status){
-
+  thread_exit();
 }
 
 // system call to create a file
@@ -95,13 +103,63 @@ read (int fd, void *buffer, unsigned size){
 // system call to write data to file descriptor
 static int 
 write (int fd, const void *buffer, unsigned size){
-
+  if (fd == 1) putbuf(buffer, size);
+  return size;
 }
 
 // system call to close file descriptor
 static void
 close (int fd){
-  
+
 }
 
 
+/*
+HELPER FUNCTIONS BELOW
+*/
+
+// Write a byte at user's virtual address provided as argument
+static bool
+put_byte(uint8_t *unsigned_dst, uint8_t byte){
+  if (is_user_vaddr(unsigned_dst)){
+    // catch error if any
+    int error;
+    asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error), "=m" (*unsigned_dst) : "q" (byte));
+    if (error == -1) return false;
+    return true;
+  }
+  return false;
+}
+
+// Read a word at at user's virtual address provided as argument
+static uint32_t
+get_word(const uint32_t *unsigned_addr){
+  if (is_user_vaddr(unsigned_addr)){
+    uint32_t result;
+    int byte, i;
+    // 4 byte words
+    for (i=0;i<4;i++){
+      byte = get_byte((uint8_t *)unsigned_addr+i);
+      if (byte != -1){
+        *((uint8_t *) &result+i) = (uint8_t) byte;
+      }
+      // failed to get byte
+      exit(-1);
+    }
+  }
+  exit(-1);
+}
+
+// Read a byte at user's virtual address provided as argument
+static int
+get_byte(const uint8_t *unsigned_addr){
+  if (is_user_vaddr(unsigned_addr)){
+    int result;
+    asm("movl $1f, %0; movzbl %1, %0; 1:"
+      : "=&a" (result) : "m" (unsigned_addr));
+    return result;
+  }
+  // when not user memory
+  return -1;
+}
